efe8309b29089bca6382a58eccccc828b5a2800c
Agraj Garg <54469328+agrajgarg@users.noreply.github.com>
2024-07-13 13:35:49 +0530

    Update urls.js
    The query parameters are appended conditionally based on whether the dev flag is set or not, ensuring correct URL formatting.

--- refactor/urls.js
+++ refactor/urls.js
@@ -4,9 +4,9 @@ function getURL(dev = false, tasks = {}) {
     let url = dev
         ? `/tasks?status=ACTIVE&dev=true&size=20`
         : '/tasks';
-
     if (tasks.nextTasks) {
-        url += '?hasNext=true';
+        url+= dev ? '&' : '?';
+        url+= 'hasNext=true';
     }
 
     if (tasks.prevTasks) {
