f1b2c2f80dcc9b017210a071dcf5ad7244793999
vinaykanth-s <vinaykanth.edu@gmail.com>
2024-07-13 22:35:46 +0530

    handled query parameter formatting

--- refactor/urls.js
+++ refactor/urls.js
@@ -1,16 +1,20 @@
-// Refactor this code
-
 function getURL(dev = false, tasks = {}) {
-    let url = dev
-        ? `/tasks?status=ACTIVE&dev=true&size=20`
-        : '/tasks';
+  let baseURL = "/tasks";
+  const params = new URLSearchParams();
 
-    if (tasks.nextTasks) {
-        url += '?hasNext=true';
-    }
+  if (dev) {
+    params.append("status", "ACTIVE");
+    params.append("dev", true);
+    params.append("size", 20);
+  }
 
-    if (tasks.prevTasks) {
-        url = '/tasks?hasPrev=true';
-    }
-    return { url };
+  if (tasks.nextTasks) {
+    params.append("hasNext", "true");
+  }
+
+  if (tasks.prevTasks) {
+    params.append("hasPrev", "true");
+  }
+
+  return { url: `${baseURL}?${params.toString()}` };
 }
