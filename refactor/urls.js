9ec03adccbb8024856cac4df09631ec054c56c5a
prayush21 <prayushdave@gmail.com>
2024-07-13 13:54:53 +0530

    Code Cleanup

--- refactor/urls.js
+++ refactor/urls.js
@@ -1,24 +1,18 @@
-// Refactor this code
-
 function getURL(dev = false, tasks = {}) {
   let url = "/tasks";
 
-  //   let url = dev ? `/tasks?status=ACTIVE&dev=true&size=20` : "/tasks";
   let params = new URLSearchParams();
   if (dev) {
-    // url += "?status=ACTIVE&dev=dev&size=20";
     params.append("status", "ACTIVE");
     params.append("dev", "true");
     params.append("size", "20");
   }
 
   if (tasks.nextTasks) {
-    // url += "?hasNext=true";
     params.append("hasNext", "true");
   }
 
   if (tasks.prevTasks) {
-    // url += "?hasPrev=true";
     params.append("hasPrev", "true");
   }
   url += `?${params.toString()}`;
