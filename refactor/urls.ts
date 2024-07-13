59c1658c57da9007db12e70a9436b47a31ace6a2
Maanil Verma <vermaMaanil97@gmail.com>
2024-07-13 13:20:13 +0530

    fix: add null check

--- refactor/urls.ts
+++ refactor/urls.ts
@@ -24,25 +24,27 @@ function getURL(
   const params = new URLSearchParams();
 
   if (dev) {
-    Object.entries(DEV_PARAMS).forEach(([key, value]) => {
-      params.append(key, value);
+    Object?.entries(DEV_PARAMS)?.forEach(([key, value]) => {
+      params?.append(key, value);
     });
   }
 
-  if (tasks.nextTasks) {
-    params.append("hasNext", "true");
+  if (tasks?.nextTasks) {
+    params?.append("hasNext", "true");
   }
 
-  if (tasks.prevTasks) {
-    params.delete("hasNext");
-    params.append("hasPrev", "true");
+  if (tasks?.prevTasks) {
+    params?.delete("hasNext");
+    params?.append("hasPrev", "true");
   }
 
-  Object.entries(additionalParams).forEach(([key, value]) => {
-    params.append(key, value);
+  Object?.entries(additionalParams)?.forEach(([key, value]) => {
+    params?.append(key, value);
   });
 
-  const url = params.toString() ? `${BASE_URL}?${params.toString()}` : BASE_URL;
+  const url = params?.toString()
+    ? `${BASE_URL}?${params?.toString()}`
+    : BASE_URL;
 
   return { url };
 }
