8a0c6a9549540a331484ce45f5b565eb76eace70
prayush21 <prayushdave@gmail.com>
2024-07-13 13:27:45 +0530

    Code Review Challenge: Use URL API

--- refactor/urls.js
+++ refactor/urls.js
@@ -1,16 +1,28 @@
 // Refactor this code
 
 function getURL(dev = false, tasks = {}) {
-    let url = dev
-        ? `/tasks?status=ACTIVE&dev=true&size=20`
-        : '/tasks';
+  let url = "/tasks";
 
-    if (tasks.nextTasks) {
-        url += '?hasNext=true';
-    }
+  //   let url = dev ? `/tasks?status=ACTIVE&dev=true&size=20` : "/tasks";
+  let params = new URLSearchParams();
+  if (dev) {
+    // url += "?status=ACTIVE&dev=dev&size=20";
+    params.append("status", "ACTIVE");
+    params.append("dev", "true");
+    params.append("size", "20");
+  }
 
-    if (tasks.prevTasks) {
-        url = '/tasks?hasPrev=true';
-    }
-    return { url };
+  if (tasks.nextTasks) {
+    // url += "?hasNext=true";
+    params.append("hasNext", "true");
+  }
+
+  if (tasks.prevTasks) {
+    // url += "?hasPrev=true";
+    params.append("hasPrev", "true");
+  }
+  url += `?${params.toString()}`;
+  return { url };
 }
+
+console.log(getURL(false, {}));
