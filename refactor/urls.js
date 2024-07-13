1ced1d03dc00077fcee12d331c4249b4f02f2ce9
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2024-07-13 13:58:08 +0530

    format

--- refactor/urls.js
+++ refactor/urls.js
@@ -1,20 +1,20 @@
 function getUrl(dev = false, tasks = {}) {
 	let url = dev
-					? `/tasks?status=active&size=20&dev=${dev}`
-					: `/tasks`;
+		? `/tasks?status=active&size=20&dev=${dev}`
+		: `/tasks`;
 
 	if (tasks.nextTasks && tasks.prevTasks) {
-					url += `${dev ? '&' : '?'}prev=true&next=true`;
-					return url;
+		url += `${dev ? '&' : '?'}prev=true&next=true`;
+		return url;
 	}
 
 	if (tasks.prevTasks) {
-					url += `${dev ? '&' : '?'}prev=true`;
+		url += `${dev ? '&' : '?'}prev=true`;
 	}
 
 	if (tasks.nextTasks) {
-					url += `${dev ? '&' : '?'}next=true`;
+		url += `${dev ? '&' : '?'}next=true`;
 	}
-	
+
 	return url;
 }
