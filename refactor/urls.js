3ebb6b0f2a3e474af1684f1338712574de48f99c
Saksham Jain <140953618+sailingsam@users.noreply.github.com>
2024-07-16 22:08:29 +0530

    Update urls.js

--- refactor/urls.js
+++ refactor/urls.js
@@ -1,16 +1,31 @@
 // Refactor this code
 
 function getURL(dev = false, tasks = {}) {
-    let url = dev
-        ? `/tasks?status=ACTIVE&dev=true&size=20`
-        : '/tasks';
-
+    const urlparams = new URLSearchParams();
+    if (dev) {
+        urlparams.append('status', 'ACTIVE');
+        urlparams.append('dev', 'true');
+        urlparams.append('size', '20');
+    }
     if (tasks.nextTasks) {
-        url += '?hasNext=true';
+        urlparams.append('hasNext', 'true');
+    } else if (tasks.prevTasks) {
+        urlparams.append('hasPrev', 'true');
     }
 
-    if (tasks.prevTasks) {
-        url = '/tasks?hasPrev=true';
-    }
-    return { url };
+    const url = `/tasks?${urlparams.toString()}`;
+    return url;
 }
+    // let url = dev
+    //     ? `/tasks?status=ACTIVE&dev=true&size=20`
+    //     : '/tasks';
+
+    // if (tasks.nextTasks) {
+    //     url += '?hasNext=true';
+    // }
+
+    // if (tasks.prevTasks) {
+    //     url = '/tasks?hasPrev=true';
+    // }
+    // return { url };
+// }
