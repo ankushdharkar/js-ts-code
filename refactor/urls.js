0ae8196cf6c5b2b9618c3270694f42d1c0c27910
ayushanand308 <ayushanand_mc21a11_34@dtu.ac.in>
2024-10-15 12:42:22 +0530

    Fix variable scope issue in getURL function

--- refactor/urls.js
+++ refactor/urls.js
@@ -1,12 +1,12 @@
 function getURL(dev = false, tasks = {}) {
     const BASE_URL = '/tasks';
-  
+    let url;
     if (tasks.prevTasks) {
         url = `${BASE_URL}?hasPrev=true`;
         return { url };
     }
   
-    let url = dev 
+    url = dev 
         ? `${BASE_URL}?status=ACTIVE&dev=true&size=20`
         : BASE_URL;
   
